04276627fc1322db0b2050d1d9438514c4c74903
gravo-lan <alvintsolh@gmail.com>
2023-02-23 14:47:25 +0800

    hide survey navbar

--- src/application.tsx
+++ src/application.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
-import { Card, CardBody, Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
+import React from 'react';
+import { Card, CardBody, Container} from 'reactstrap';
 import SurveyComponent from './components/survey';
 import defaultSurveyConfig from './config/survey';
 
 export interface IApplicationProps {}
 
 const Application: React.FunctionComponent<IApplicationProps> = props => {
-    const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
 
     return (
         <>
-            <Navbar color="primary" dark expand="md">
-                <Container>
-                    <NavbarBrand>Survey Website</NavbarBrand>
-                    <NavbarToggler onClick={() => setNavbarOpen(!navbarOpen)} />
-                    <Collapse isOpen={navbarOpen} navbar>
-                        <Nav className="me-auto" navbar></Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
             <Container>
                 <Card className="mt-1">
                     <CardBody>
